306660b35185a9a15893968f1a7a79e1bf245838
shengboxu <shengboxu@indeed.com>
2018-10-27 18:31:04 +0900

    add: init

---
+++ src/main/java/life/sideworks/abc111a/Main.java
@@ -0,0 +1,26 @@
+package life.sideworks.abc111a;
+
+import java.util.Scanner;
+
+/**
+ * @author shengboxu
+ */
+
+public class Main {
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        // 整数の入力
+        int a = sc.nextInt();
+
+        // スペース区切りの整数の入力
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // 文字列の入力
+        String s = sc.next();
+
+        // 出力
+        System.out.println((a+b+c) + " " + s);
+    }
+}
